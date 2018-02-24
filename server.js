b615c105871a72f4cd879c21c9385c821ff18cdb
Teega <ricardo.ortega79@gmail.com>
2018-02-24 09:02:22 -0700

    mongoose connection change

--- server.js
+++ server.js
@@ -30,7 +30,10 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/week18Populater", {
+useMongoClient: true
+ });
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
