c0fd2e2c2823d05d2f40b73543b63a3e29e5a026
Teega <ricardo.ortega79@gmail.com>
2018-02-24 13:32:27 -0700

    modified the mongodb heroku connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var exphbs = require("express-handlebars");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -30,10 +30,10 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI);
-mongoose.connect("mongodb://localhost/week18Populater", {
-useMongoClient: true
- });
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/week18Populater", {
+// useMongoClient: true
+//  });
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
